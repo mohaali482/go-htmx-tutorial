24af69ef4368ff242a093c0fed2613f8c1ace83f
Mohammed Ali <mohaali482@gmail.com>
2023-12-10 17:38:28 +0300

    Add database functions

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,50 +1,96 @@
 package database
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
 	"log"
 	"os"
-	"time"
 
 	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Service interface {
-	Health() map[string]string
+	CreateCount() CountType
+	GetAllCounts() ([]CountType, error)
+	GetCount(id int) (CountType, error)
+	IncrementCount(id int) error
+	DecrementCount(id int) error
+	DeleteCount(id int) error
 }
 
 type service struct {
-	db *sql.DB
+	db *gorm.DB
 }
 
 var (
 	dburl = os.Getenv("DB_URL")
 )
 
+type CountType struct {
+	Id    int
+	Value int
+}
+
+type Count struct {
+	gorm.Model
+	Value int
+}
+
+func (c Count) ToCountType() CountType {
+	return CountType{Id: int(c.ID), Value: c.Value}
+}
+
 func New() Service {
-	db, err := sql.Open("sqlite3", dburl)
+	db, err := gorm.Open(sqlite.Open(dburl), &gorm.Config{})
+	db.AutoMigrate(&Count{})
 	if err != nil {
-		// This will not be a connection error, but a DSN parse error or
-		// another initialization error.
 		log.Fatal(err)
 	}
 	s := &service{db: db}
 	return s
 }
 
-func (s *service) Health() map[string]string {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
+func (s *service) CreateCount() CountType {
+	count := Count{Value: 0}
+	s.db.Create(&count)
+	return count.ToCountType()
+}
 
-	err := s.db.PingContext(ctx)
-	if err != nil {
-		log.Fatalf(fmt.Sprintf("db down: %v", err))
+func (s *service) GetAllCounts() ([]CountType, error) {
+	var counts []Count
+	s.db.Find(&counts)
+	var countTypes []CountType
+	for _, count := range counts {
+		countTypes = append(countTypes, count.ToCountType())
 	}
+	return countTypes, nil
+}
 
-	return map[string]string{
-		"message": "It's healthy",
-	}
+func (s *service) GetCount(id int) (CountType, error) {
+	var count Count
+	s.db.First(&count, id)
+	countType := count.ToCountType()
+	return countType, nil
+}
+
+func (s *service) IncrementCount(id int) error {
+	var count Count
+	s.db.First(&count, id)
+	count.Value++
+	s.db.Save(&count)
+	return nil
+}
+
+func (s *service) DecrementCount(id int) error {
+	var count Count
+	s.db.First(&count, id)
+	count.Value--
+	s.db.Save(&count)
+	return nil
+}
+
+func (s *service) DeleteCount(id int) error {
+	var count Count
+	return s.db.Delete(&count, id).Error
 }
