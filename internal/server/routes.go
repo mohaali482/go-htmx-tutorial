b3f4de09c32e04b7e23149294205a9e5a2a75f79
Mohammed Ali <mohaali482@gmail.com>
2023-12-10 17:41:59 +0300

    Add count routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,18 +1,63 @@
 package server
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func (s *FiberServer) RegisterFiberRoutes() {
 	s.App.Get("/", s.index)
-	s.App.Get("/health", s.healthHandler)
+	s.App.Post("/create", s.createCount)
+	s.App.Delete("/delete/:id", s.deleteCount)
+	s.App.Post("/increment/:id", s.incrementCount)
+	s.App.Post("/decrement/:id", s.decrementCount)
 }
 
 func (s *FiberServer) index(c *fiber.Ctx) error {
-	return c.Render("index", fiber.Map{})
+	counts, _ := s.db.GetAllCounts()
+	return c.Render("index", fiber.Map{"counts": counts})
 }
 
-func (s *FiberServer) healthHandler(c *fiber.Ctx) error {
-	return c.JSON(s.db.Health())
+func (s *FiberServer) createCount(c *fiber.Ctx) error {
+	count := s.db.CreateCount()
+	return c.Render("count", fiber.Map{"count": count})
+}
+
+func (s *FiberServer) incrementCount(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	s.db.IncrementCount(id)
+	count, _ := s.db.GetCount(id)
+	return c.Render("count", fiber.Map{"count": count})
+}
+
+func (s *FiberServer) decrementCount(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	err = s.db.DecrementCount(id)
+	if err != nil {
+		return c.SendStatus(fiber.StatusNotFound)
+	}
+	count, err := s.db.GetCount(id)
+	if err != nil {
+		return c.SendStatus(fiber.StatusNotFound)
+	}
+	return c.Render("count", fiber.Map{"count": count})
+}
+
+func (s *FiberServer) deleteCount(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	err = s.db.DeleteCount(id)
+	if err != nil {
+		return c.SendStatus(fiber.StatusNotFound)
+	}
+	return c.SendStatus(fiber.StatusOK)
 }
